b2ac1cce08e863752ff4e45efb81424a5c98b80d
zienmaeia190-bit <zienmaeia190@gmail.com>
2026-07-25 23:13:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Bot is running!'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+Thread(target=run).start()
+
 import subprocess, sys
 
 # ─── تثبيت المكتبات تلقائياً ───
